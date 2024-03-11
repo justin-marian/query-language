93ad6d78bbb001aac1f7f1e792c607658437f83e
Justin-Marian Popescu <pmarianjustin@gmail.com>
2024-03-11 23:29:46 +0200

    Added object Util Table and Test unit.

---
+++ src/main/scala/util/Util.scala
@@ -0,0 +1,8 @@
+package util
+
+object Util {
+  // Row: Map with column name as key and value as value
+  type Row = Map[String, String]
+  // Line: List of values without column names
+  type Line = List[String]
+}
